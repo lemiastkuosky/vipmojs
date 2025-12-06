74ba3584b3f9890729c9f969ae2668663e4b173b
lemiastkuosky <le.miastkuosky@live.com>
2025-12-05 21:04:29 -0300

    Create dados.php

---
+++ supertrunfo/dados.php
@@ -0,0 +1,13 @@
+<?php
+// dados.php
+function getBaralho() {
+    return [
+        ['nome' => 'Ferrari F8', 'img' => 'https://via.placeholder.com/250x150/ff0000/fff?text=Ferrari', 'kmh' => 340, 'hp' => 720, 'acc' => 2.9, 'peso' => 1330],
+        ['nome' => 'Lambo Aventador', 'img' => 'https://via.placeholder.com/250x150/ffff00/000?text=Lamborghini', 'kmh' => 350, 'hp' => 740, 'acc' => 2.9, 'peso' => 1575],
+        ['nome' => 'Porsche 911', 'img' => 'https://via.placeholder.com/250x150/999999/000?text=Porsche', 'kmh' => 330, 'hp' => 650, 'acc' => 2.7, 'peso' => 1640],
+        ['nome' => 'McLaren 720S', 'img' => 'https://via.placeholder.com/250x150/ff8800/fff?text=McLaren', 'kmh' => 341, 'hp' => 720, 'acc' => 2.9, 'peso' => 1283],
+        ['nome' => 'Bugatti Chiron', 'img' => 'https://via.placeholder.com/250x150/0000ff/fff?text=Bugatti', 'kmh' => 420, 'hp' => 1500, 'acc' => 2.4, 'peso' => 1996],
+        ['nome' => 'Ford GT', 'img' => 'https://via.placeholder.com/250x150/0033cc/fff?text=Ford+GT', 'kmh' => 347, 'hp' => 660, 'acc' => 3.0, 'peso' => 1385],
+        // Adicione novos carros aqui facilmente...
+    ];
+}
